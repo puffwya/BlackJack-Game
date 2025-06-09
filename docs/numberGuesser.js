458ef0a6a1576dce3089f446222966f39026d179
Wyatt Puff <puffwya000@gmail.com>
2025-06-09 15:47:57 -0400

    Updated numberGuesser

--- docs/numberGuesser.js
+++ docs/numberGuesser.js
@@ -1,33 +1,25 @@
-const randomNumber = Math.floor(Math.random() * 10) + 1;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 
 function guess() {
-
   const usrInput = parseFloat(document.getElementById("usrGuess").value);
   let result;
 
   if (isNaN(usrInput)) {
-    result = "Please enter a number 1-10.";
-  }
-  else if (usrInput == randomNumber) {
-    result = "Correct guess!;
-  }
-  else if (usrInput < randomNumber) {
+    result = "Please enter a number 1–10.";
+  } else if (usrInput == randomNumber) {
+    result = "Correct guess!";
+  } else if (usrInput < randomNumber) {
     result = "Too Low.";
-  }
-  else if (usrInput > randomNumber){
+  } else {
     result = "Too High.";
   }
 
   document.getElementById("result").innerText = result;
-
 }
 
 function resetGame() {
- 
   randomNumber = Math.floor(Math.random() * 10) + 1;
- 
-  document.getElementById("result").innerText = "";
- 
   document.getElementById("usrGuess").value = "";
-
+  document.getElementById("result").innerText = "";
 }
+
